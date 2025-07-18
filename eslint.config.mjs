dbf2f28a1a66e745999f4ebc2be2c769d0cf96a4
adelsultann <adelalsultan647@gmail.com>
2025-07-18 15:53:39 +0300

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import ts from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,26 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  // Base configs
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Override or add TypeScript rules
+  {
+    plugins: {
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      // Customize no-unused-vars for TypeScript
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
